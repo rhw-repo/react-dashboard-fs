2015c38fb6897709b7bbc117d8f0308b3f554e4e
Ruth Westnidge Brown <85512549+rhw-repo@users.noreply.github.com>
2026-02-19 17:49:34 +0000

    Save WIP. Add possible breakpoints with max-width rules for the container containing the table in the DemoPage component

--- frontend/src/components/ui/data-table/page.tsx
+++ frontend/src/components/ui/data-table/page.tsx
@@ -6,6 +6,7 @@ import { DataTable } from "./data-table";
 export default function DemoPage() {
   "use no memo";
   return (
+    //<div className="mx-auto py-10 max-w-sm sm:max-w-7xl md:max-w-7xl lg:max-w-7-xl">
     <div className="container mx-auto py-10">
       <DataTable columns={columns} data={person} />
     </div>
