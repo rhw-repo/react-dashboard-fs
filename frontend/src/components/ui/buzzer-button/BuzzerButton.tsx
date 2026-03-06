755898ea279c9c4657a6fc1188862332c2e7f3e8
Ruth Westnidge Brown <85512549+rhw-repo@users.noreply.github.com>
2026-03-06 14:40:26 +0000

    Add second button to demonstrate animation for active state (orange glowing ring).

--- frontend/src/components/ui/buzzer-button/BuzzerButton.tsx
+++ frontend/src/components/ui/buzzer-button/BuzzerButton.tsx
@@ -4,7 +4,16 @@ const BuzzerButton = () => {
   return (
     <section className={styles.wrapper}>
       <button className={styles.sphere}>
-        <span className={styles.sphere__front}>
+        <span>
+          {' '}
+          PRESS THE BIG
+          <br />
+          ORANGE BUTTON
+        </span>
+      </button>
+
+      <button className={`${styles.fxSphere} ${styles.sphere}`}>
+        <span>
           {' '}
           PRESS THE BIG
           <br />
