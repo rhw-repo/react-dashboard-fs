e213a4b11a9690e531ac89b4082ff9148c558cbf
Ruth Westnidge Brown <85512549+rhw-repo@users.noreply.github.com>
2026-03-06 16:39:49 +0000

    Save WIP. Add SignUpCard with link to LoginCard.

--- frontend/src/components/login-card/LoginCard.tsx
+++ frontend/src/components/login-card/LoginCard.tsx
@@ -1,14 +1,4 @@
-import {
-  Card,
-  CardAction,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from '@/components/ui/card';
-
-//import { Label } from '@/components/ui/label';
+import { Card, CardAction, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card';
 import { Button } from '@/components/ui/button';
 import { NavLink } from 'react-router';
 
@@ -24,32 +14,7 @@ export function LoginCard() {
           </NavLink>
         </CardAction>
       </CardHeader>
-      <CardContent>
-        <form>
-          <div className="flex flex-col gap-6">
-            {/*<div className="grid gap-2">
-              <Label htmlFor="email">Email</Label>
-              <Input id="email" type="email" placeholder="m@example.com" required />
-            </div>*/}
-
-            {/* 
-             <div className="grid gap-2">
-             <div className="flex items-center">
-                <Label htmlFor="password">Password</Label>
-                <a href="#" className="ml-auto inline-block text-sm underline-offset-4 hover:underline">
-                  Forgot your password?
-                </a>
-              </div>
-              <Input id="password" type="password" required />
-            </div>
-            */}
-          </div>
-        </form>
-      </CardContent>
       <CardFooter className="flex-col gap-2">
-        {/*<Button type="submit" className="w-full">
-          Login
-        </Button>*/}
         <Button variant="outline" className="w-full">
           Login with Google
         </Button>
--- frontend/src/components/signup-card/SignUpCard.tsx
+++ frontend/src/components/signup-card/SignUpCard.tsx
@@ -1,5 +1,24 @@
-function SignUpCard() {
-  return <h1>Test</h1>;
-}
+import { Card, CardAction, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card';
+import { Button } from '@/components/ui/button';
+import { NavLink } from 'react-router';
 
-export default SignUpCard;
+export default function SignupCard() {
+  return (
+    <Card className="mx-4 w-full max-w-sm">
+      <CardHeader>
+        <CardTitle>Signup for an account</CardTitle>
+        <CardDescription>Use Login if you have an account</CardDescription>
+        <CardAction>
+          <NavLink to="/login">
+            <Button variant="link">Login</Button>
+          </NavLink>
+        </CardAction>
+      </CardHeader>
+      <CardFooter className="flex-col gap-2">
+        <Button variant="outline" className="w-full">
+          Signup with Google
+        </Button>
+      </CardFooter>
+    </Card>
+  );
+}
