b162e9d95a0179c1b49a160d6ff5dc73be421105
Ruth Westnidge Brown <85512549+rhw-repo@users.noreply.github.com>
2026-03-09 16:16:28 +0000

    Initial commit on this branch save WIP. Position new clipped second sphere to be the rim, eliinate the existing rim which was created with a box shadow and couldn't be adequately styled to give a rounded appearance.

--- frontend/src/components/ui/buzzer-button/BuzzerButton.tsx
+++ frontend/src/components/ui/buzzer-button/BuzzerButton.tsx
@@ -1,10 +1,10 @@
 import styles from './BuzzerButton.module.css';
 
-const BuzzerButton = () => {
-  return (
-    <section className={styles.wrapper}>
-      {/* Simply 3d hover & active animations */}
-      {/*
+{
+  /* Simply 3d hover & active animations */
+}
+{
+  /*
    <button className={styles.sphere}>
         <span>
           {' '}
@@ -13,25 +13,42 @@ const BuzzerButton = () => {
           ORANGE BUTTON
         </span>
       </button>
-      {/* Plain ring animation on active */}
-      {/* <button className={`${styles.fxSphere} ${styles.sphere}`}>
+      {/* Plain ring animation on active */
+}
+{
+  /* <button className={`${styles.fxSphere} ${styles.sphere}`}>
         <span>
           {' '}
           PRESS THE BIG
           <br />
           ORANGE BUTTON
         </span>
-      </button> */}
-      {/* WINNER! Fiery ring animation on active */}
-      <button className={`${styles.sphere} ${styles['fxSphere--fire']}`}>
-        <span>
-          {' '}
-          PRESS THE BIG
-          <br />
-          ORANGE BUTTON
-        </span>
-      </button>
-    </section>
+      </button> */
+}
+
+{
+  /* WINNER! Fiery ring animation on active */
+}
+const BuzzerButton = () => {
+  return (
+    <>
+      <section className={styles.wrapper}>
+        <div className={styles.buttonShell}>
+          <div className={styles.rim}>
+            <span className={styles.rimShadow}></span>
+          </div>
+
+          <button className={`${styles.sphere} ${styles['fxSphere--fire']}`}>
+            <span>
+              {' '}
+              PRESS THE BIG
+              <br />
+              ORANGE BUTTON
+            </span>
+          </button>
+        </div>
+      </section>
+    </>
   );
 };
 
