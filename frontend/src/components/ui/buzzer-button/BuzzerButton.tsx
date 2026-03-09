7f0dc10771e779e8dd1f72fa77963362fc8e6829
Ruth Westnidge Brown <85512549+rhw-repo@users.noreply.github.com>
2026-03-09 19:00:44 +0000

    Squash merge of dev after it's own merge of feature/improve-button branch

--- frontend/src/components/ui/buzzer-button/BuzzerButton.tsx
+++ frontend/src/components/ui/buzzer-button/BuzzerButton.tsx
@@ -2,36 +2,24 @@ import styles from './BuzzerButton.module.css';
 
 const BuzzerButton = () => {
   return (
-    <section className={styles.wrapper}>
-      {/* Simply 3d hover & active animations */}
-      {/*
-   <button className={styles.sphere}>
-        <span>
-          {' '}
-          PRESS THE BIG
-          <br />
-          ORANGE BUTTON
-        </span>
-      </button>
-      {/* Plain ring animation on active */}
-      {/* <button className={`${styles.fxSphere} ${styles.sphere}`}>
-        <span>
-          {' '}
-          PRESS THE BIG
-          <br />
-          ORANGE BUTTON
-        </span>
-      </button> */}
-      {/* WINNER! Fiery ring animation on active */}
-      <button className={`${styles.sphere} ${styles['fxSphere--fire']}`}>
-        <span>
-          {' '}
-          PRESS THE BIG
-          <br />
-          ORANGE BUTTON
-        </span>
-      </button>
-    </section>
+    <>
+      <section className={styles.buttonWrapper}>
+        <div className={styles.buttonShell}>
+          <div className={styles.buttonRim}>
+            <span className={styles.buttonRimShadow}></span>
+          </div>
+
+          <button className={`${styles.buttonTop} ${styles['buttonTop--fire']}`}>
+            <span>
+              {' '}
+              PRESS THE BIG
+              <br />
+              ORANGE BUTTON
+            </span>
+          </button>
+        </div>
+      </section>
+    </>
   );
 };
 
