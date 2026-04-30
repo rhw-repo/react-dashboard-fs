e469d72a29e041d6a196f8ea784c66dd66d0eb8f
Ruth Westnidge Brown <85512549+rhw-repo@users.noreply.github.com>
2026-04-30 11:12:00 +0000

    Add responsiveness to text and images in ErrorFallback UI. Prevent overflow & disproportionate font size & font weight. Add line breaks in the {content.title} within the .ts file

--- frontend/src/components/ui/error-fallback-ui/ErrorFallbackUI.tsx
+++ frontend/src/components/ui/error-fallback-ui/ErrorFallbackUI.tsx
@@ -12,24 +12,24 @@ interface ErrorFallbackUIProps {
 // TODO - images and text content SB tbc
 export const ErrorFallbackUI = ({ onAction, content }: ErrorFallbackUIProps) => {
   return (
-    <div className="flex min-h-screen w-full items-center justify-center bg-slate-50 p-4 sm:p-8 md:p-12 dark:bg-slate-950">
-      <Card className="flex min-h-[75vh] w-full max-w-5xl flex-col overflow-hidden border-slate-300 bg-slate-700 text-slate-50 shadow-xl">
+    <main className="flex min-h-screen w-full items-center justify-center bg-slate-50 p-4 sm:p-8 md:p-12 dark:bg-slate-950">
+      <Card className="flex min-h-[75vh] w-full max-w-5xl flex-col overflow-hidden border-slate-300 bg-slate-850 text-slate-50 shadow-xl">
         <CardContent className="flex flex-1 flex-col items-center justify-center p-8 text-center sm:p-12 md:p-16">
           {content.imageSrc && (
             <img
               src={content.imageSrc}
               alt={content.imageAlt}
-              className="mx-auto mb-6 max-w-sm rounded-2xl"
+              className="mx-auto mb-6 w-full h-auto max-w-sm rounded-2xl object-cover"
             />
           )}
-          <h2 className="mb-8 text-6xl tracking-wide whitespace-pre-line">
+          <h1 className="mb-8 text-4xl font-normal sm:font-light sm:text-6xl text-neutral-100 tracking-wide whitespace-pre-line">
            {content.title}
-          </h2>
+          </h1>
 
-          <p className="py-4 text-3xl font-semibold tracking-wide">{content.description}</p>
-          <p className="pt-4 pb-16 text-3xl font-semibold tracking-wide">
+          <h2 className="py-4 text-2xl sm:text-3xl font-normal tracking-wide text-neutral-100">{content.description}</h2>
+          <h3 className="pt-4 pb-16 text-3xl sm:text-4xl font-normal tracking-wide text-yellow-300">
             {content.instruction}
-          </p>
+          </h3>
           <Button
             onClick={onAction}
             className="rounded-sm border border-neutral-50 bg-blue-950 p-8 text-2xl text-neutral-50 transition-colors hover:bg-blue-900"
@@ -38,6 +38,6 @@ export const ErrorFallbackUI = ({ onAction, content }: ErrorFallbackUIProps) => 
           </Button>
         </CardContent>
       </Card>
-    </div>
+    </main>
   );
 };
