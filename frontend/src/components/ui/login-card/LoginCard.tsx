ac1aecf32bd0d371fd058ea4ad07526ed83be197
Ruth Westnidge <85512549+rhw-repo@users.noreply.github.com>
2026-05-06 11:17:09 +0100

    Feature/react error boundary CSS fixes (#3)
    * Add px-20 to grid container for whole page prevent overflow-x caused by the second article having no constraint
    
    * Save WIP: Fix layout to enable whole page to scroll on x asix as per SB request, instead of individual elements gaining scrolls
    
    * SAve WIP. Prevent image from resizing and moving to the left when screen widths reduced by adding in  w-max min-w-full to the article containing the tables, fixing the width and change image to inline block to experiment with stabilising the block. TODO: experiment with change image to flex
    
    * Save WIP: height of 50vh for second article to keep height consistent
    
    * Style the color, font-size, font-weight of the chart title for better contrast
    
    * Style fontFamily for the chart title set to NotoSans for unifrmity
    
    * Add overlay instead of p to caption the temp placeholder image
    
    * Set identical fontweights to chart title and placeholder overlay text for uniformity
    
    * Add 4px of padding to the element with  data-slot navigation-link-menu. Improve layout by adding space between the first link and top of page
    
    * Increase people contacted to test layout
    
    * Add y axis padding of 1rem to home, improve spacing. Center the signup and login cards
    
    * Set font-family Karla as base font, avoids repetition, overrides for th. thead, li for headings and links in Noto Sants font. Fix canonical class warning in TaskTimelinePage. Center both login & signup cards, add a h1 title to explain purpose. Improves understanding & layout

--- frontend/src/components/ui/login-card/LoginCard.tsx
+++ frontend/src/components/ui/login-card/LoginCard.tsx
@@ -4,21 +4,24 @@ import { NavLink } from 'react-router';
 
 export function LoginCard() {
   return (
-    <Card className="mx-4 w-full max-w-sm">
-      <CardHeader>
-        <CardTitle>Login to your account</CardTitle>
-        <CardDescription>Login with Google to access your account</CardDescription>
-        <CardAction>
-          <NavLink to="/signup">
-            <Button variant="link">Sign Up</Button>
-          </NavLink>
-        </CardAction>
-      </CardHeader>
-      <CardFooter className="flex-col gap-2">
-        <Button variant="outline" className="w-full">
-          Login with Google
-        </Button>
-      </CardFooter>
-    </Card>
+    <article className="flex flex-col min-h-screen items-center justify-center">
+      <h1 className='p-4 text-neutral-50 font-medium font-noto-sans'>Shadcn card, SSO anticipated with Google</h1>
+      <Card className="mx-4 w-full max-w-sm font-noto-sans">
+        <CardHeader>
+          <CardTitle>Login to your account</CardTitle>
+          <CardDescription>Login with Google to access your account</CardDescription>
+          <CardAction>
+            <NavLink to="/signup">
+              <Button variant="link">Sign Up</Button>
+            </NavLink>
+          </CardAction>
+        </CardHeader>
+        <CardFooter className="flex-col gap-2">
+          <Button variant="outline" className="w-full">
+            Login with Google
+          </Button>
+        </CardFooter>
+      </Card>
+    </article>
   );
 }
