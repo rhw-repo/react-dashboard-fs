bcd1a42b52d0a40d0e14f65106a7f856f0bc4180
Ruth Westnidge Brown <85512549+rhw-repo@users.noreply.github.com>
2026-06-02 14:51:44 +0000

    Save WIP - fix page scroll without distorting layout

--- frontend/src/components/ui/task-timeline-table/TaskTimelinePage.tsx
+++ frontend/src/components/ui/task-timeline-table/TaskTimelinePage.tsx
@@ -6,36 +6,37 @@ import { TaskTimelineSectionWrapper } from './TaskTimeLineTablesSection';
 export default function TaskTimeLinePage() {
   'use no memo';
   return (
-    
-    <div className="mx-auto grid min-h-screen max-w-550 grid-cols-[9rem_1fr]  border-2 border-transparent [border-image:linear-gradient(to_top,#4f46e5,#18181b)_1]">
+    <div className="h-screen w-full overflow-auto">
+    <div className="mx-auto grid h-max min-h-full w-max min-w-[1200px] max-w-550 grid-cols-[9rem_1fr] border-2 border-transparent [border-image:linear-gradient(to_top,#4f46e5,#18181b)_1]">
       <aside className="justify-self-start">
         <Navbar />{' '}
       </aside>
 
-      <main className="col-start-2 h-full w-max min-w-full py-10">
+      <main className="col-start-2 min-h-full  min-w-full py-10">
         <article className="flex w-full gap-4">
-          <div className="min-w-0 flex-1 px-20">
+          <div className="flex-1 px-20">
             <TaskTimelineSectionWrapper />
           </div>
         </article>
 
-        <article className="mt-8 grid h-[50vh] w-full grid-cols-2 gap-4">
-          <div className="col-span-1 col-start-1 h-full min-w-0">
+        <article className="mt-24 grid h-[550px] w-full grid-cols-2 gap-4 px-20">
+          <div className="col-span-1 col-start-1 h-full">
             <BurnUpChart />
           </div>
 
-          <div className="relative col-span-1 col-start-2 inline-block h-full w-237.5 align-top shadow-xl shadow-indigo-500/50">
+          <div className="relative col-span-1 col-start-2 h-full shadow-xl shadow-indigo-500/50">
             <p className="absolute top-4 left-4 z-10 rounded bg-black/60 px-2 py-1 text-[17px] font-extralight text-white backdrop-blur-sm">
               Placeholder for Kanban Board
             </p>
             <img
               src="https://mintcdn.com/kan/tZr6SCXtNIaMjnC7/images/hero-dark.png?w=2500&fit=max&auto=format&n=tZr6SCXtNIaMjnC7&q=85&s=e3c16964a05107ab04b31add4a7efa47"
               alt="Temporary placeholder image of kanban board to demo the layout"
-              className="h-full w-full max-w-none rounded-md bg-muted/20 object-contain"
+              className="absolute inset-0 h-full w-full max-w-none rounded-md bg-muted/20 object-contain"
             />
           </div>
         </article>
       </main>
+    </div>
     </div>
   );
 }
