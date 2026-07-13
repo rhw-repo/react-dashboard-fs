fe754efda52728f0e314022a158575d67ab89723
Ruth Westnidge Brown <85512549+rhw-repo@users.noreply.github.com>
2026-07-13 15:00:57 +0100

    Fix: make FileUploaderProps readonly to make sure immutable. Prevent child components having posibility to modify which coould cause unexpected behaviour

--- frontend/src/components/ui/record-edit-modal/FileUploader.tsx
+++ frontend/src/components/ui/record-edit-modal/FileUploader.tsx
@@ -1,26 +1,26 @@
-import '@uppy/react/css/style.css'
+import '@uppy/react/css/style.css';
 
-import type Uppy from '@uppy/core'
-import { UppyContextProvider, Dropzone, FilesList } from '@uppy/react'
+import type Uppy from '@uppy/core';
+import { UppyContextProvider, Dropzone, FilesList } from '@uppy/react';
 
 export type UploadedFile = {
-  uploadURL: string
-  fileName: string
-  fileType: string
-  fileSize: number
-}
+  uploadURL: string;
+  fileName: string;
+  fileType: string;
+  fileSize: number;
+};
 
 type FileUploaderProps = {
-  uppy: Uppy
-}
+  readonly uppy: Uppy;
+};
 
 export function FileUploader({ uppy }: FileUploaderProps) {
   return (
-    <section className="mt-6 space-y-3 [--uppy-color-gray-50:transparent] [--uppy-color-gray-300:var(--color-indigo-500)] [--uppy-color-gray-500:var(--color-neutral-50)] [--uppy-color-gray-600:var(--color-neutral-50)] [--uppy-color-blue-50:transparent]">
+    <section className="mt-6 space-y-3 [--uppy-color-blue-50:transparent] [--uppy-color-gray-50:transparent] [--uppy-color-gray-300:var(--color-indigo-500)] [--uppy-color-gray-500:var(--color-neutral-50)] [--uppy-color-gray-600:var(--color-neutral-50)]">
       <UppyContextProvider uppy={uppy}>
         <Dropzone height="180px" note="Images, PDFs and documents up to 50 MB" />
         <FilesList />
       </UppyContextProvider>
     </section>
-  )
+  );
 }
